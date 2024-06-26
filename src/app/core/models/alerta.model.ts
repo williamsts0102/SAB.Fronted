2ddbd1381c98814763d7d88f1ff42f3411daec54
Williams Toro Susanibar <82621773+williamsts0102@users.noreply.github.com>
2024-06-26 12:46:22 -0500

    arreglos

--- src/app/core/models/alerta.model.ts
+++ src/app/core/models/alerta.model.ts
@@ -3,8 +3,10 @@ export interface Alerta {
   strDepartamento: string;
   strProvincia: string;
   strDistrito: string;
+  strDireccion: string;
   strDescripcion: string;
   strLatitud: string;
   strLongitud: string;
+  intIdGrupoPersonal: number;
   bitEstado: boolean;
 }
